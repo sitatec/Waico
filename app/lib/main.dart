c2386c8c9d32f03866fbbc9415c2606770d79f6a
Sita Bérété <sita.berete.3@gmail.com>
2025-07-16 05:37:55 +0000

    [app] Properly init DB

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:waico/app.dart';
+import 'package:waico/core/services/database/db.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DB.init();
   runApp(const App());
 }
