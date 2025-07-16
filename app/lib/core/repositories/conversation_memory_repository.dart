5140dfde9d4c31b144ec261f2c9cdac4410c71db
Sita Bérété <sita.berete.3@gmail.com>
2025-07-16 22:55:28 +0000

    [app] Fix: Make repository operations async

--- app/lib/core/repositories/conversation_memory_repository.dart
+++ app/lib/core/repositories/conversation_memory_repository.dart
@@ -8,19 +8,19 @@ import 'package:waico/generated/objectbox.g.dart';
 class ConversationMemoryRepository extends ObjectBoxBaseRepository<ConversationMemory> {
   ConversationMemoryRepository() : super(DB.provider.getBox<ConversationMemory>());
 
-  List<ConversationMemory> searchMemories({required List<double> queryVector, int maxResultCount = 10}) {
+  Future<List<ConversationMemory>> searchMemories({required List<double> queryVector, int maxResultCount = 5}) async {
     // TODO: output both the conversation and the score and fine-tuned the LLM to ajust it's confidence
     // of the remembered memory based on the score and respond to the user accordingly (uncertain for low scores,...)
 
     // Passing higher maxResultCount to the vector search but limit the result to the requested maxResultCount
     // Can improve quality according to docs.
-    final maxVectorSearchCount = min(maxResultCount * 5, 100);
+    final maxVectorSearchCount = min(maxResultCount * 10, 100);
     final query = box
         .query(ConversationMemory_.embeddings.nearestNeighborsF32(queryVector, maxVectorSearchCount))
         .build();
     query.limit = maxResultCount;
 
-    final results = query.findWithScores();
+    final results = await query.findWithScoresAsync();
 
     final eligibleConversations = results
         .where((result) => result.score > ConversationMemory.embeddingsScoreThreshold)
--- app/lib/core/repositories/conversation_repository.dart
+++ app/lib/core/repositories/conversation_repository.dart
@@ -6,9 +6,9 @@ import 'package:waico/generated/objectbox.g.dart';
 class ConversationRepository extends ObjectBoxBaseRepository<Conversation> {
   ConversationRepository() : super(DB.provider.getBox<Conversation>());
 
-  List<Conversation> getLatestConversations({required int count}) {
+  Future<List<Conversation>> getLatestConversations({required int count}) {
     final query = box.query().order(Conversation_.updatedAt, flags: Order.descending).build();
     query.limit = count;
-    return query.find();
+    return query.findAsync();
   }
 }
